4d6638e4159612c8ca70b8ccbed3f90e88daea9f
Lakhan <lakhan.mango@gmail.com>
2024-08-24 18:15:43 +0530

    updated

---
+++ pages/cars/[id].js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { Container, Row } from "react-bootstrap";
+
+const fetchCar = async (id) => {
+  const { data } = await axios.get(
+    `http://localhost:3003/api/cars?id=${escape(id)}`
+  );
+  return data;
+};
+
+export async function getServerSideProps(context) {
+  const data = await fetchCar(context.params.id);
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
+
+const Car = ({ data }) => {
+  return (
+    <div>
+      <Container>
+        {data && (
+          <>
+            <Row>
+              <img
+                src={`/cars/${data.id}.jpg`}
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Row>
+            <Row>
+              <h1 className="text-center">{(data && data.name) || "Cars"}</h1>
+            </Row>
+          </>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default Car;
